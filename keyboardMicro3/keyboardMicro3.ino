ceb76f6a9ecf96be99c1cc32272b43fc95a1aa3c
Saul <saulc@users.noreply.github.com>
2022-08-06 15:24:54 -0700

    clean up
    ...

--- keyboardMicro3/keyboardMicro3.ino
+++ keyboardMicro3/keyboardMicro3.ino
@@ -1,19 +1,18 @@
 /*
+ * mini keyboard - code keys 3 x 3
   micro based keyboard 3 interupts.  7 pinchange interupt? 6!
   pins: int: 2,3,7 pcint: 8,9,10, 14, 15, 16   
-  spaceL  mc  spaceR  
-  comment uncomment ([0, 0, 0]) 'custom'
+  spaceL  mc  spaceR        macOS
+  comment uncomment ([0, 0, 0]) 'custom openscad'
   tab/return  copy/cut  paste/paste+return
     
 */ 
+
 #include "Keyboard.h"
 #include "button.h"
  void cclick();
  void aclick();
- void bclick();
- void spaceL();
- void spaceR();
- void dt(int);
+ void bclick(); 
  void one();
  void two();
  void three();
@@ -27,13 +26,12 @@ Button a3(10, three, false);
 Button b1(14, four, false);
 Button b2(15, five, false);
 Button b3(16, six, false);
-
 Button x(2, aclick, true);
 Button c(3, bclick, true);
 Button v(7, cclick, true);
 
-
 const int d = 11; //release delay
+//interupt callbacks
 void cclick(){ 
     int t = v.tap();
     if(t==1)  dt(0);
@@ -49,11 +47,38 @@ void bclick(){
     if(t==1)  commandKey(1); 
     else if(t == 2) commandKey(0);
 }    
- 
+ //pin change interupt callbacks
+void one(void){
+    int t = a1.tap();
+    if(t==1)  macro("([0, 0, 0]) ");        
+//    else if(t == 2) dt();
+}
+void two(void){
+    int t = a2.tap();
+    if(t==1) commandKey(3); 
+}
+void three(void){
+    int t = a3.tap();
+    if(t==1) commandKey(4); //  
+    else if(t == 2) commandKey(5); 
+}   
+void four(void){
+    int t = b1.tap();
+    if(t==1) spaceR();
+}
+void five(void){
+    int t = b2.tap();
+    if(t==1) space(2);
+}
+void six(void){ 
+    int t = b3.tap();
+    if(t==1) spaceL();
+}   
+
 void setup() { 
+  //turn off the builtin led
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
-
    //setup interupts for buttons
    v.ini();  
    c.ini(); 
@@ -64,19 +89,12 @@ void setup() {
    b1.ini();
    b2.ini();
    b3.ini();
-
-//      pinMode(2, INPUT_PULLUP); 
   // initialize control over the keyboard:
   Keyboard.begin();
 }
-
-int p = 2;
-
-int t = 0;
+ 
 void loop() {  
-//  if( digitalRead(p) == LOW) spaceL();    //to do test non interupt buttons.
-  
-//  if(t++ > 1000)    Keyboard.releaseAll(); //fixes command click after wake.!
+//  RGB control goes here lol
 }
 
 void commandKey(int a){
@@ -88,68 +106,41 @@ void commandKey(int a){
         break;
       case 2: c = 'v';
         break;
-        case 3: c = '/';
+        case 3: c = 'd';
         break; 
-        case 4: c = 'd';
+        case 4: c = 'd';    
+        Keyboard.press(KEY_RIGHT_SHIFT); 
+        break;
+        case 5: c = '/';
         break;
     }
-    
 //    Keyboard.press(KEY_LEFT_CTRL);  // press and hold control windoz
-  if(a==4)  Keyboard.press(KEY_RIGHT_SHIFT);  //  hold Shift for testing!! 
- 
+//    Keyboard.press(KEY_RIGHT_SHIFT);  //  hold Shift for testing!! 
     Keyboard.press(KEY_LEFT_GUI);  // press and hold command macOS not X
     Keyboard.press(c);         
-
     kbrelease();
 }
-
+//change spaces/destops
 void spaceL(){ space(0); }
 void spaceR(){ space(1); }
 void space(int d){   
     Keyboard.press(KEY_LEFT_CTRL);  // press and hold control windoz 
      if(d==2) Keyboard.press(KEY_UP_ARROW);   //spaces/mission control
-     else    Keyboard.press( (d==0) ? KEY_LEFT_ARROW : KEY_RIGHT_ARROW);      //change spaces/destops
+     else    Keyboard.press( (d==0) ? KEY_LEFT_ARROW : KEY_RIGHT_ARROW);      
     
     kbrelease();
 }
- 
+ //single key shortcuts
 void dt(int a){  
     if(a == 0) Keyboard.press(KEY_TAB); 
     else if(a == 1) Keyboard.press(KEY_RETURN);  
     else if(a == 2) Keyboard.press(KEY_BACKSPACE);  
     kbrelease();
 }
+//write keystrokes 
 void macro(String s){
   for(int i=0; i<s.length()-1; i++)
            Keyboard.write(s[i]); 
-}
-void one(void){
-    int t = a1.tap();
-    if(t==1) {
-      String s = "([0, 0, 0]) ";
-      macro(s);
-    }  
-//    else if(t == 2) dt();
-}
-void two(void){
-    int t = a2.tap();
-    if(t==1) commandKey(3);
-}
-void three(void){
-    int t = a3.tap();
-    if(t==1) commandKey(4); // dt(2); //del
-}
-void four(void){
-    int t = b1.tap();
-    if(t==1) spaceR();
-}
-void five(void){
-    int t = b2.tap();
-    if(t==1) space(2);
-}
-void six(void){
-    int t = b3.tap();
-    if(t==1) spaceL();
 }
 void kbrelease(){
     delay(d);  //fixes command click after wake.!
